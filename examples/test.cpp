ee67f3e96f385f34370948fbcc53c71add0a733a
nick <goodfella005@gmail.com>
2013-06-13 11:32:40 -0700

    Added a more thorough test program:
    * This test covers the following cases
    
      + Field that's byte aligned at the start and end and fits in a byte.
      + Field that's byte aligned at the start and end and spans multiple bytes.
    
      + Field that's not byte aligned at the start, but byte aligned at the end, and fits in a byte.
      + Field that's not byte aligned at the start, but byte aligned at the end, and spans multiple bytes.
    
      + Field that's byte aligned at the start, but not byte aligned at the end, and fits in a byte.
      + Field that's byte aligned at the start, but not byte aligned at the end, and spans mutliple bytes.
    
      + Field that's not byte aligned at the start, not byte aligned at the end, and fits in a byte.
      + field that's not byte aligned at the start, not byte aligned at the end, and spans multiple bytes.

---
+++ examples/test.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <tuple>
+#include <cstring>
+#include "protocol_helper.hpp"
+
+using namespace std;
+using namespace protocol_helper;
+
+typedef tuple<
+    field<8, unsigned short>, // field 0
+    field<1, bool>,           // field 1
+    field<6, unsigned short>, // field 2
+    field<17, unsigned int>,  // field 3
+    field<9, unsigned short>, // field 4
+    field<7, unsigned short>, // field 5
+    field<16, unsigned int>,  // field 6
+    field<1, bool>,           // field 7
+    field<7, unsigned short>, // field 8
+    field<1, bool>,           // field 9
+    field<9, unsigned short>  // field 10
+    > test;
+
+typedef protocol<test> test_proto;
+
+int main()
+{
+    unsigned char buf[11];
+
+    memset(buf, 0, sizeof(buf));
+
+    buf[0] = 0x47;  // field 0
+    buf[1] = 0x80;  // field 1
+    buf[1] |= 0x7e; // field 2
+    buf[1] |= 0x01; // beginning of field 3
+    buf[2] = 0x01;  // middle of field 3
+    buf[3] = 0x01;  // end of field 3
+    buf[4] = 0xff;  // beginning of field 4
+    buf[5] = 0x80;  // end of field 4
+    buf[5] |= 0x7f; // field 5
+    buf[6] = 0x91;  // beginning of field 6
+    buf[7] = 0x91;  // end of field 6
+    buf[8] = 0x80;  // field 7
+    buf[8] |= 0x7e; // field 8
+    buf[9] |= 0x80; // field 9
+    buf[9] |= 0x7f; // beginning of field 10
+    buf[10] = 0xff; // end of field 10
+
+    cout << "field 0: " << std::hex << std::showbase << test_proto::get_field<0>(buf) << endl;
+    cout << "field 1: " << std::hex << std::showbase << test_proto::get_field<1>(buf) << endl;
+    cout << "field 2: " << std::hex << std::showbase << test_proto::get_field<2>(buf) << endl;
+    cout << "field 3: " << std::hex << std::showbase << test_proto::get_field<3>(buf) << endl;
+    cout << "field 4: " << std::hex << std::showbase << test_proto::get_field<4>(buf) << endl;
+    cout << "field 5: " << std::hex << std::showbase << test_proto::get_field<5>(buf) << endl;
+    cout << "field 6: " << std::hex << std::showbase << test_proto::get_field<6>(buf) << endl;
+    cout << "field 7: " << std::hex << std::showbase << test_proto::get_field<7>(buf) << endl;
+    cout << "field 8: " << std::hex << std::showbase << test_proto::get_field<8>(buf) << endl;
+    cout << "field 9: " << std::hex << std::showbase << test_proto::get_field<9>(buf) << endl;
+    cout << "field 10: " << std::hex << std::showbase << test_proto::get_field<10>(buf) << endl;
+}
