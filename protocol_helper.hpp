f2690f5b0b294503ed9dae12774639360e0d9aea
nick <goodfella005@gmail.com>
2013-06-13 01:36:15 -0700

    Fix get_field with 1 bit and 8 bit fields:
    * Note that this may still be broken.

--- protocol_helper.hpp
+++ protocol_helper.hpp
@@ -55,38 +55,20 @@ namespace protocol_helper
 	enum : size_t { value = bit_offset<I, Tuple>::value / 8 };
     };
 
-    /// Returns the mask necessary to select the number of bits from the right
-    /**
-     *  @tparam Bits the number of bits from the right of an unsigned char
-     *  to select
-     */
-    template<size_t Bits>
-    struct rbit_mask
-    {
-	enum : unsigned char { value = (1 << (Bits - 1)) + rbit_mask<Bits - 1>::value };
-    };
-
-    /// Base implementation of rbit_mask
-    template<>
-    struct rbit_mask<0>
-    {
-	enum : unsigned char { value = 0 };
-    };
-
     /// Returns the mask necessary to select the number of bits from the left
     /**
      *  @tparam Bits the number of bits from the left of an unsigned char
      *  to select
      */
-    template<size_t Bits>
+    template<size_t Bits, size_t Start>
     struct lbit_mask
     {
-	enum : unsigned char { value = (1 << (8 - Bits)) + lbit_mask<Bits - 1>::value };
+	enum : unsigned char { value = (1 << (8 - (Bits + Start))) + lbit_mask<Bits - 1, Start>::value };
     };
 
     /// Base implementation of lbit_mask
-    template<>
-    struct lbit_mask<0>
+    template<size_t Start>
+    struct lbit_mask<0, Start>
     {
 	enum: unsigned char { value = 0 };
     };
@@ -112,12 +94,30 @@ namespace protocol_helper
 	}
     };
 
+    /// Specialization of get_subbyte when gt8 is true
+    template<class T>
+    struct get_subbyte<true, 0, T>
+    {
+	static const T value(unsigned char const * const buf) {
+	    return 0;
+	}
+    };
+
     /// Specialization of get_subbyte when gt8 is false
     template<size_t Field_Bits, class T>
     struct get_subbyte<false, Field_Bits, T>
     {
 	static const T value(unsigned char const * const buf) {
-	    return buf[0] & lbit_mask<Field_Bits>::value;
+	    return buf[0] & lbit_mask<Field_Bits, 0>::value;
+	}
+    };
+
+    /// Specialization of get_subbyte when gt8 is false
+    template<class T>
+    struct get_subbyte<false, 0, T>
+    {
+	static const T value(unsigned char const * const buf) {
+	    return 0;
 	}
     };
 
@@ -131,9 +131,12 @@ namespace protocol_helper
     struct get_field
     {
 	static const T value(unsigned char const * const buf) {
-	    const size_t bits = 8 - (Field_Offset % 8);
+	    // What bit to start the bitmask
+	    const size_t start = Field_Offset % 8;
+	    // How many bits to include in the bitmask
+	    const size_t bits = Field_Bits > 8 ? Field_Bits % 8 : Field_Bits;
 	    return
-		((buf[0] & rbit_mask<bits>::value) << 8) +
+		((buf[0] & lbit_mask<bits, start>::value) << (Field_Bits > bits ? Field_Bits - bits : 0)) +
 		get_subbyte<(Field_Bits - bits > 8), Field_Bits - bits, T>::value(buf + 1);
         }
     };
